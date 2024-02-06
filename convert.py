b3c753e5acd84a4be455ec12bfd6f5b88e46acf0
Gabe Krishnadasan <gkrishnadasan@sandiego.edu>
2024-02-05 18:18:09 -0800

    Gathered all data from excel sheet

--- convert.py
+++ convert.py
@@ -3,28 +3,55 @@ import openpyxl as opxl
 
 wb = opxl.load_workbook("Membership Proposal Details.xlsx")
 
-
-
-
 class company:
-    def __init__(self, fullName, companyName, email, phoneNumber, membershipType, space, description, term, startDate, endDate, rate, additionalFees, discountPromo, misc, link):
-        self.fullName = fullName
-        self.companyName = companyName
-        self.email = email
-        self.phoneNumber = phoneNumber
-        self.membershipType = membershipType
-        self.space = space
-        self.description = description
-        self.term = term
-        self.startDate = startDate
-        self.endDate = endDate
-        self.rate = rate
-        self.additionalFees = additionalFees
-        self.discountPromo = discountPromo
-        self.misc = misc
-        self.link = link
+    def __init__(self):
+        self.fullName = ""
+        self.companyName = ""
+        self.email = ""
+        self.phoneNumber = ""
+        self.membershipType = ""
+        self.space = ""
+        self.description = ""
+        self.term = ""
+        self.startDate = ""
+        self.endDate = ""
+        self.rate = ""
+        self.additionalFees = ""
+        self.discountPromo = ""
+        self.misc = ""
+        self.link = ""
     
 
-def gatherInfo():
-    pass
+def gatherInfo(obj, sheetname):
+    wb = opxl.load_workbook(sheetname)
+    wb._active_sheet_index = 0
+    ws = wb.active
+
+    obj.fullName = ws["B1"].value
+    obj.companyName = ws["B2"].value
+    obj.email = ws["B3"].value
+    obj.phoneNumber = ws["B4"].value
+
+    wb._active_sheet_index = 1
+    ws = wb.active
+
+    obj.membershipType = ws["B1"].value
+    obj.space = ws["B3"].value
+    obj.description = ws["B4"].value
+    obj.term = ws["B5"].value
+    obj.startDate = ws["B6"].value
+    obj.endDate = ws["B7"].value
+    obj.rate = ws["B8"].value
+    obj.additionalFees = ws["B9"].value
+    obj.discountPromo = ws["B10"].value
+    obj.misc = ws["B12"].value
+    obj.link = ws["B17"].value
+
+
+obj = company()
+sheetname = "Membership Proposal Details.xlsx"
+
+gatherInfo(obj, sheetname)
+
+print(obj.fullName)
 
